5a37a29b821c28e8788eacc6f9ba7d14d96b7748
Ashley Mathew <amathew195@gmail.com>
2022-10-21 13:46:25 -0500

    added homepage route and homepage.html

--- models.py
+++ models.py
@@ -14,3 +14,25 @@ def connect_db(app):
     app.app_context().push()
     db.app = app
     db.init_app(app)
+
+class Pet(db.Model):
+    """Pet"""
+
+    __tablename__='pets'
+
+    id = db.Column(db.Integer,
+                    primary_key=True,
+                    autoincrement=True)
+    name = db.Column(db.String(50),
+                    nullable=False)
+    species = db.Column(db.String(50),
+                    nullable=False)
+    photo_url = db.Column(db.Text,
+                    nullable=False,
+                    default="")
+    age = db.Column(db.String(2),
+                    nullable=False)
+    notes = db.Colum(db.String(100))
+    available = db.Column(db.Boolean,
+                    nullable=False,
+                    default=True)
